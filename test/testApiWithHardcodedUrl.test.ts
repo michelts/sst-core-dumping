0e1ea10907293d68220e47035d6a50040295207d
Michel Sabchuk <michel@turbosys.com.br>
2023-04-05 15:45:00 -0300

    Update hardcoded url (for me) and add shortcuts for thread/no-thread

--- test/testApiWithHardcodedUrl.test.ts
+++ test/testApiWithHardcodedUrl.test.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import { expect, it } from 'vitest';
 
 it('API test', async () => {
-  const response = await fetch('https://i64u4st2t2.execute-api.us-east-1.amazonaws.com')
+  const response = await fetch('https://ls3h8ove70.execute-api.us-east-1.amazonaws.com');
   const data = await response.json();
   expect(data.body).toMatch(/hello world/i);
 });
